4a228cca43d2c7a5c97d7ab509540ff1f1fcad3f
ladicz <lada.buchta@gmail.com>
2025-07-18 14:59:54 +0200

    Update datepicker.blade.php
    readonly

--- resources/views/components/datepicker.blade.php
+++ resources/views/components/datepicker.blade.php
@@ -3,7 +3,8 @@
         x-init="init()"
         class="w-full"
     >
-        <input type="text"
+        <input readonly
+            type="text"
             id="my-picker"
             x-ref="picker"
             class="datepicker bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
